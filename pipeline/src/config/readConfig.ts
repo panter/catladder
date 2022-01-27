11cbe14f4c7cbfd7f3d066906dd9ab348b0e9e0f
Marco Wettstein <maw@panter.ch>
2022-01-27 15:33:24 +0100

    fix

--- pipeline/src/config/readConfig.ts
+++ pipeline/src/config/readConfig.ts
@@ -29,7 +29,7 @@ export const readConfigSync = (
     } else {
       return load(
         readFileSync(fullPath(directory, found), { encoding: "utf-8" })
-      );
+      ) as Config;
     }
   }
   return null;
