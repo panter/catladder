73f39da533a5cc312a74e052619688ccb9bf4652
Marco Wettstein <maw@panter.ch>
2022-04-06 17:10:51 +0200

    fix: values.secretsAsFile not working properly

---
+++ pipeline/src/deploy/kubernetes/processSecretsAsFiles.ts
@@ -0,0 +1,34 @@
+import { omit } from "lodash";
+import { DeployConfigKubernetesValues } from "../types";
+
+export const processSecretsAsFiles = <
+  T extends {
+    env: {
+      secret: Record<string, string>;
+    };
+  } & DeployConfigKubernetesValues
+>(
+  values: T
+): T => {
+  if (!values.secretsAsFile || values.secretsAsFile.length === 0) {
+    return values;
+  }
+  // the initial idea was that we "shadow" secrets that we mount as files,
+  // but there is a weird kubernetes  issue with that (https://github.com/kubernetes/kubernetes/issues/46861),
+  // so instead we rename the secrets and append _content, so that inside kuberentes, we can use the original name to contain the path and name+_content as the actual content
+  return {
+    ...values,
+    env: {
+      ...values.env,
+      secret: {
+        ...omit(values.env.secret, values.secretsAsFile),
+        ...Object.fromEntries(
+          values.secretsAsFile.map((key) => [
+            key + "_content",
+            values.env.secret[key],
+          ])
+        ),
+      },
+    },
+  };
+};
