5866927de30ffd34221225a122e62f4b407ff943
Marco Wettstein <maw@panter.ch>
2022-04-08 12:18:35 +0200

    fix: catenv hangs

--- cli/src/catenv.ts
+++ cli/src/catenv.ts
@@ -1,4 +1,7 @@
 import catenv from "./apps/catenv/catenv";
 import { $ } from "zx";
 $.verbose = false;
-catenv();
+catenv().then(() => {
+  // we have to exit manually, because we have some file watches
+  process.exit();
+});
