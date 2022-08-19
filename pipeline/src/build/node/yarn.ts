3a787db2545e6942a9b6399f13094be68c8f892a
Marco Wettstein <maw@panter.ch>
2022-08-19 09:26:06 +0200

    fix: build broken with yarn 4 or newer

--- pipeline/src/build/node/yarn.ts
+++ pipeline/src/build/node/yarn.ts
@@ -6,18 +6,29 @@ export const getYarnInstallCommand = (
     prodOnly?: boolean;
     noScripts?: boolean;
   }
-) =>
-  context.packageManagerInfo?.isClassic
-    ? `yarn install --frozen-lockfile ${
-        options?.prodOnly ? "--production" : ""
-      } ${options?.noScripts ? "--ignore-scripts" : ""}`
-    : options?.prodOnly
-    ? `${
-        options?.noScripts ? "YARN_ENABLE_SCRIPTS=false " : ""
-      }yarn plugin import workspace-tools && yarn workspaces focus --production` // needs yarn plugin import workspace-tools
+) => {
+  if (context.packageManagerInfo?.isClassic) {
+    return `yarn install --frozen-lockfile ${
+      options?.prodOnly ? "--production" : ""
+    } ${options?.noScripts ? "--ignore-scripts" : ""}`;
+  }
+  // is modern
+
+  // yarn >= 4 ships with build in plugins, see https://github.com/yarnpkg/berry/pull/4253
+  // trying to import those fail on this version
+  const doesNotShipWithBuiltInPlugins = ["2", "3"].some((v) =>
+    context.packageManagerInfo?.version.startsWith(v)
+  );
+  return options?.prodOnly
+    ? `${options?.noScripts ? "YARN_ENABLE_SCRIPTS=false " : ""}${
+        doesNotShipWithBuiltInPlugins
+          ? "yarn plugin import workspace-tools && "
+          : " "
+      }yarn workspaces focus --production` // needs yarn plugin import workspace-tools
     : `${
         options?.noScripts ? "YARN_ENABLE_SCRIPTS=false " : ""
       }yarn install --immutable`;
+};
 
 export const ensureNodeVersion = (context: Context) => [
   "if [ -f ./.nvmrc ]; then source /root/.nvm/nvm.sh && nvm install <<< .nvmrc; fi",
