80da6721fae8111c6b91f31881e8f8ba37c520b6
Marco Wettstein <maw@panter.ch>
2022-09-19 21:54:46 +0200

    feat(cloudrun): use fullName as service name

--- pipeline/src/deploy/cloudRun/deployJob.ts
+++ pipeline/src/deploy/cloudRun/deployJob.ts
@@ -39,7 +39,7 @@ export const createDeployJob = (context: Context): CatladderJob[] => {
     GCLOUD_DEPLOY_CREDENTIALS_KEY
   );
 
-  const serviceName = context.environment.slug;
+  const serviceName = context.environment.fullName;
 
   const labelsString = Object.entries(getLabels(context))
     .map(([key, value]) => `${key}=${value}`)
