ecee5eea51feadb56a40d68c270f155be431a910
Marco Wettstein <maw@panter.ch>
2022-09-20 17:35:29 +0200

    feat: remove update notifier
    update notifier made some troubles, so i removed it for the time being

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -1,8 +1,3 @@
-import updateNotifier from "update-notifier";
 import cli from "./apps/cli/cli";
-import packageInfos from "./packageInfos";
 
-updateNotifier({
-  pkg: packageInfos,
-}).notify();
 cli();
