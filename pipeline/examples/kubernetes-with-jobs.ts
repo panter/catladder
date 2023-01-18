3804c25e6600a65750309a870dd4ac3a6e88359a
Marco Wettstein <maw@panter.ch>
2023-01-18 16:24:56 +0100

    chore: add new example and snapshot

---
+++ pipeline/examples/kubernetes-with-jobs.ts
@@ -0,0 +1,57 @@
+import type { Config } from "../src";
+import { createAllPipelines } from "./__utils__/helpers";
+const config: Config = {
+  appName: "test-app",
+  customerName: "pan",
+  components: {
+    api: {
+      dir: "api",
+      build: {
+        type: "node",
+      },
+      deploy: {
+        type: "kubernetes",
+        cluster: {
+          name: "some-cluster-name",
+          region: "europe-west6",
+          projectId: "some-project-id",
+          type: "gcloud",
+          domainCanonical: "panter.cloud",
+        },
+        values: {
+          jobs: {
+            migration: {
+              command: "yarn migrate",
+            },
+          },
+          cronjobs: {
+            ["send-emails"]: {
+              command: "yarn send emails",
+              schedule: "0 * * * *",
+            },
+          },
+        },
+      },
+    },
+    www: {
+      dir: "www",
+      build: {
+        type: "node",
+      },
+      deploy: {
+        type: "kubernetes",
+        cluster: {
+          name: "some-cluster-name",
+          region: "europe-west6",
+          projectId: "some-project-id",
+          type: "gcloud",
+          domainCanonical: "panter.cloud",
+        },
+      },
+    },
+  },
+};
+
+it("matches snapshot", async () => {
+  expect(await createAllPipelines(config)).toMatchSnapshot();
+});
