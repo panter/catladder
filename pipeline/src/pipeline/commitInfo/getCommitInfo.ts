18cb8d8614d51acc8a16e6b7150f57d4764160a8
Michael Leu <leu@panter.ch>
2023-07-06 09:46:35 +0200

    fix(pipeline): prefix hotflix slug with alpha chars to conform to k8s specs

--- pipeline/src/pipeline/commitInfo/getCommitInfo.ts
+++ pipeline/src/pipeline/commitInfo/getCommitInfo.ts
@@ -6,7 +6,9 @@ export const getBaseCommitInfo = async (): Promise<
   refName: process.env.CI_COMMIT_REF_NAME ?? "unknown",
   reviewSlug: process.env.CI_MERGE_REQUEST_IID
     ? `mr${process.env.CI_MERGE_REQUEST_IID}`
-    : process.env.CI_COMMIT_REF_SLUG ?? "unknown",
+    : process.env.CI_COMMIT_REF_SLUG
+    ? `hf${process.env.CI_COMMIT_REF_SLUG}`
+    : "unknown",
   buildTime: new Date().toISOString(),
   buildId: await getBuildId(),
   currentVersion: await getCurrentVersionString(),
