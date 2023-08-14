8f79dd9fbc2da315994add25071d013c6e0f81df
Kaspar Vollenweider <vok@panter.ch>
2023-08-14 14:30:50 +0200

    test(custom-build): add little bit of tests for artifact merge

---
+++ pipeline/src/build/base/__tests__/createArtifactsConfig.test.ts
@@ -0,0 +1,41 @@
+import { createArtifactsConfig } from "../createArtifactsConfig";
+
+describe("createArtifactsConfig", () => {
+  it("returns undefined if no artifacts or artifactsReports", () => {
+    expect(createArtifactsConfig(".")).toBeUndefined();
+  });
+
+  it("returns artifacts if no artifactsReports", () => {
+    expect(
+      createArtifactsConfig(".", undefined, {
+        paths: ["testpath"],
+        reports: {
+          coverage_report: { coverage_format: "cobertura", path: "some" },
+          terraform: "something",
+        },
+      })
+    ).toMatchSnapshot();
+  });
+
+  it("returns junit parts of artifactsReports", () => {
+    expect(
+      createArtifactsConfig(".", { junit: ["report.xml"] })
+    ).toMatchSnapshot();
+  });
+
+  it("merges junit files if both given", () => {
+    const result = createArtifactsConfig(
+      ".",
+      { junit: ["partial-report.xml"] },
+      {
+        paths: ["testpath"],
+        reports: {
+          junit: "full-report.xml",
+          terraform: "something",
+        },
+      }
+    );
+    expect(result).toMatchSnapshot();
+    expect(result?.artifacts?.reports?.junit).toHaveLength(2);
+  });
+});
---
+++ pipeline/src/build/custom/__tests__/testJob.test.ts
@@ -0,0 +1,32 @@
+import type { Context } from "../../..";
+import { createCustomTestJobs } from "../testJob";
+
+describe("createCustomTestJobs", () => {
+  const componentConfig: Context["componentConfig"] = {
+    dir: ".",
+    build: { type: "custom", docker: { type: "custom" }, jobImage: "" },
+    deploy: {},
+  } as Context["componentConfig"];
+  const baseContext: Context = {
+    componentName: "testComponent",
+    environment: {
+      jobOnlyVars: {
+        build: {},
+      },
+    },
+    componentConfig,
+  } as Context;
+
+  it("throws error when not build type custom", () => {
+    componentConfig.build.type = "node";
+    expect(() => createCustomTestJobs(baseContext)).toThrowError(
+      "deploy config is not custom"
+    );
+    componentConfig.build.type = "custom";
+  });
+
+  it("returns empty array if no audit, lint, and test definition", () => {
+    const jobs = createCustomTestJobs(baseContext);
+    expect(jobs).toEqual([]);
+  });
+});
