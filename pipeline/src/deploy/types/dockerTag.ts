022bac0917b983e119c88ea95e5ff086f9f9e78c
Michael Leu <leu@panter.ch>
2023-08-28 16:30:37 +0200

    docs(pipeline): add hint about the drawbacks of the dockerTag deploy type

--- pipeline/src/deploy/types/dockerTag.ts
+++ pipeline/src/deploy/types/dockerTag.ts
@@ -5,6 +5,8 @@ export type DeployConfigDockerTag = {
    * adds a custom tag on the repo to deploy it.
    *
    * only used in veloplus atm, generally not recommended to use
+   * because run-time environment variables have to be coordinated
+   * manually with the platform running the container.
    */
   type: "dockerTag";
 
