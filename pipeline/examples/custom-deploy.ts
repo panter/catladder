ad0dcbbbec972c5e266d26e1fa3eada14b4106ae
Marco Wettstein <maw@panter.ch>
2023-10-03 15:04:43 +0200

    docs: add example to increase memory and cpu for the runner

--- pipeline/examples/custom-deploy.ts
+++ pipeline/examples/custom-deploy.ts
@@ -15,6 +15,14 @@ const config: Config = {
         requiresDocker: true,
         jobVars: {
           secret: ["DEPLOY_API_KEY"],
+
+          public: {
+            // if your deploy job requires more memory and/or cpu, you can increase them with those variables:
+            KUBERNETES_CPU_REQUEST: "1",
+            KUBERNETES_CPU_LIMIT: "1",
+            KUBERNETES_MEMORY_REQUEST: "1024Mi",
+            KUBERNETES_MEMORY_LIMIT: "2048Mi",
+          },
         },
         script: ["echo 'would deploy'"],
         stopScript: ["echo 'would stop'"],
