7a9a76eacbfb8dc375fd56cf37e5512901b7a8ec
Michael Leu <leu@panter.ch>
2023-11-29 17:06:19 +0100

    refactor(gcloud): use bash process substitution instead of temp file

--- pipeline/src/deploy/cloudRun/utils/gcloudServiceAccountLoginCommands.ts
+++ pipeline/src/deploy/cloudRun/utils/gcloudServiceAccountLoginCommands.ts
@@ -2,13 +2,9 @@ import { GCLOUD_DEPLOY_CREDENTIALS_KEY } from "..";
 import { getSecretVarNameForContext } from "../../../context";
 import type { Context } from "../../../types";
 
-export const gcloudServiceAccountLoginCommands = (context: Context) => {
-  return [
-    `echo $${getSecretVarNameForContext(
-      context,
-      GCLOUD_DEPLOY_CREDENTIALS_KEY
-    )} > ./____keyfile.json
-          `,
-    `gcloud auth activate-service-account --key-file="./____keyfile.json"`,
-  ];
-};
+export const gcloudServiceAccountLoginCommands = (context: Context) => [
+  `gcloud auth activate-service-account --key-file=<(echo "$${getSecretVarNameForContext(
+    context,
+    GCLOUD_DEPLOY_CREDENTIALS_KEY
+  )}")`,
+];
