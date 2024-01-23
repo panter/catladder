2e3a88fa649a3ce4ab0a8a7eb2c2569ba97707ca
Michael Leu <leu@panter.ch>
2024-01-23 16:23:11 +0100

    feat(pipeline): do not create pipeline from schedules

--- pipeline/src/rules/index.ts
+++ pipeline/src/rules/index.ts
@@ -4,11 +4,18 @@ export const RULE_NEVER_ON_RELEASE_COMMIT: GitlabRule = {
   if: "$CI_COMMIT_MESSAGE =~ /^chore\\(release\\).*/",
   when: "never",
 };
+
+export const RULE_NEVER_ON_SCHEDULE: GitlabRule = {
+  if: '$CI_PIPELINE_SOURCE  == "schedule"',
+  when: "never",
+};
+
 export const RULES_ALWAYS: GitlabRule[] = [
   {
     if: "$CI_COMMIT_TAG",
   },
   RULE_NEVER_ON_RELEASE_COMMIT,
+  RULE_NEVER_ON_SCHEDULE,
   {
     if: "$CI_COMMIT_BRANCH =~ /^[0-9]+.([0-9]+|x).x$/",
   },
@@ -28,6 +35,7 @@ export const RULE_CONDITION_HOTFIX_BRANCH =
 
 export const RULES_RELEASE: GitlabRule[] = [
   RULE_NEVER_ON_RELEASE_COMMIT,
+  RULE_NEVER_ON_SCHEDULE,
   {
     if: RULE_CONDITION_MAIN_BRANCH + ' && $AUTO_RELEASE == "true"',
     when: "on_success",
@@ -44,6 +52,7 @@ export const RULES_RELEASE: GitlabRule[] = [
 
 export const RULES_MANUAL_RELEASE: GitlabRule[] = [
   RULE_NEVER_ON_RELEASE_COMMIT,
+  RULE_NEVER_ON_SCHEDULE,
   {
     if: RULE_CONDITION_MAIN_BRANCH,
     when: "manual",
