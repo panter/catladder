50277fbb3f7ec1dcc11fb2b31dac8b3e6c39bd1c
Marco Wettstein <maw@panter.ch>
2024-06-11 12:16:11 +0200

    fix: version number not displayed correctly in cli

--- cli/src/packageInfos.ts
+++ cli/src/packageInfos.ts
@@ -1,4 +1,9 @@
+// we can't import the package.json directly, because the bundling process will inline it
+// but the version is only set during deployement, when its already bundled
+// instead we need to use require.main.path to get the path of the main module, which is actually the file in the bin
+// this will point to the root package.json
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ownPkg = require("../package.json");
+const ownPkg = require(require.main.path + "/../package.json");
 
 export default ownPkg;
