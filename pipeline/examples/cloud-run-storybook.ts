ff7e4c5a7a2da372a1f1b8ab250ae775ada1a75d
Marco Wettstein <maw@panter.ch>
2024-06-14 16:30:30 +0200

    docs: add storybook example

---
+++ pipeline/examples/cloud-run-storybook.ts
@@ -0,0 +1,24 @@
+import type { Config } from "../src";
+import { createAllPipelines } from "./__utils__/helpers";
+
+const config: Config = {
+  appName: "test-app",
+  customerName: "pan",
+  components: {
+    api: {
+      dir: "app",
+      build: {
+        type: "storybook",
+      },
+      deploy: {
+        type: "google-cloudrun",
+        projectId: "asdf",
+        region: "asia-east1",
+      },
+    },
+  },
+};
+
+it("matches snapshot", async () => {
+  expect(await createAllPipelines(config)).toMatchSnapshot();
+});
