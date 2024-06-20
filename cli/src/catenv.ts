acb3bf6b3126d68af380e3ea7e4c461bc3df098d
Kaspar Vollenweider <vok@panter.ch>
2024-06-20 14:04:21 +0200

    feat(cli): catenv returns usage message and doc link on --help
    The arguments -h, --help, or help will return a help message and exit.

--- cli/src/catenv.ts
+++ cli/src/catenv.ts
@@ -2,9 +2,19 @@ import catenv from "./apps/catenv/catenv";
 
 import { parseChoice } from "./config/parseChoice";
 
-const choice = process.argv[2] ? parseChoice(process.argv[2]) : null;
+const args = process.argv.slice(2);
 
-catenv(choice).then(() => {
+const helpFlags = ["--help", "-h", "help"];
+if (args.some((arg) => helpFlags.includes(arg))) {
+  const docLink =
+    "https://git.panter.ch/catladder/catladder/-/blob/main/docs/1_VARS.md";
+  console.log(
+    `\nUsage: catenv [env|env:component]\n\nEnv variable and catenv documentation:\n${docLink}`,
+  );
+  process.exit(0);
+}
+
+catenv(args[0] ? parseChoice(args[0]) : null).then(() => {
   // we have to exit manually, because we have some file watches
   process.exit();
 });
