6dcede6c54406273523deb18eaa83f65135353c3
Marco Wettstein <maw@panter.ch>
2024-06-28 01:43:08 +0200

    fix: cache and artifacts are huge in gitlab
    fastest disabled the compression, so caches became huge. fast is a good compromise

--- pipeline/src/pipeline/gitlab/createGitlabPipeline.ts
+++ pipeline/src/pipeline/gitlab/createGitlabPipeline.ts
@@ -15,8 +15,8 @@ export const createGitlabPipelineWithDefaults = ({
     image: image ?? getRunnerImage("jobs-default"), // default image
     variables: {
       FF_USE_FASTZIP: "true", // enable fastzip - a faster zip implementation that also supports level configuration.
-      ARTIFACT_COMPRESSION_LEVEL: "fastest", // we value speed over compression
-      CACHE_COMPRESSION_LEVEL: "fastest", // same as above, but for caches
+      ARTIFACT_COMPRESSION_LEVEL: "fast", // we value speed over compression
+      CACHE_COMPRESSION_LEVEL: "fast", // same as above, but for caches
       TRANSFER_METER_FREQUENCY: "5s", // how often we should update the transfer meter for cache upload/download
 
       GIT_DEPTH: "1", // no need the full depth
