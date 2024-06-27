d644a92ed6ff67231bcacc0b3e426b40c260ff91
Marco Wettstein <maw@panter.ch>
2024-06-28 01:30:22 +0200

    perf: faster cache in gitlab

--- pipeline/src/pipeline/gitlab/createGitlabPipeline.ts
+++ pipeline/src/pipeline/gitlab/createGitlabPipeline.ts
@@ -14,7 +14,11 @@ export const createGitlabPipelineWithDefaults = ({
   return {
     image: image ?? getRunnerImage("jobs-default"), // default image
     variables: {
-      FF_USE_FASTZIP: "true",
+      FF_USE_FASTZIP: "true", // enable fastzip - a faster zip implementation that also supports level configuration.
+      ARTIFACT_COMPRESSION_LEVEL: "fastest", // we value speed over compression
+      CACHE_COMPRESSION_LEVEL: "fastest", // same as above, but for caches
+      TRANSFER_METER_FREQUENCY: "5s", // how often we should update the transfer meter for cache upload/download
+
       GIT_DEPTH: "1", // no need the full depth
       ...(variables ?? {}),
     },
