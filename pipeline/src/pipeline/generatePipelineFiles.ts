ed99589268ea8b27719aa0affd08bd93399a873a
Kaspar Vollenweider <vok@panter.ch>
2024-07-09 19:34:48 +0200

    refactor(pipeline): generatePipelineFiles split out child and local prepare in functions
    This is a preparation to create snapshot tests that test the actual generated
    YAML output with snapshots.
    
    For this, I need the data passed to writeYamlFile separately available.
    
    This is why I exported these two functions.

--- pipeline/src/pipeline/generatePipelineFiles.ts
+++ pipeline/src/pipeline/generatePipelineFiles.ts
@@ -4,48 +4,73 @@ import { createChildPipeline } from "./createChildPipeline";
 import { createMainPipeline } from "./createMainPipeline";
 import { getPipelineTriggerForGitlabChildPipeline } from "./gitlab/getPipelineTriggerForGitlabChildPipeline";
 import { sortGitLabJobDefProps } from "./gitlab/sortGitLabJobDefProps";
+
+export async function generateChildPipelineContent<T extends PipelineType>(
+  config: Config,
+  pipelineType: T,
+) {
+  const trigger = getPipelineTriggerForGitlabChildPipeline();
+  const { jobs, image, stages, variables, workflow, ...pipelineRest } =
+    await createChildPipeline(pipelineType, trigger, config);
+
+  const jobsWithSortedProps = Object.fromEntries(
+    Object.entries(jobs).map(([jobName, job]) => [
+      jobName,
+      sortGitLabJobDefProps(job),
+    ]),
+  );
+
+  return {
+    image,
+    stages,
+    variables,
+    workflow,
+    ...pipelineRest,
+    // jobs need to be spread into main YAML, because GitLab pipeline YAML has no jobs key - jobs are top level with their key as their name
+    ...jobsWithSortedProps,
+  };
+}
+
+export async function generateLocalPipelineContent<T extends PipelineType>(
+  config: Config,
+  pipelineType: T,
+) {
+  const { jobs, image, stages, variables, workflow, ...pipelineRest } =
+    await createMainPipeline(pipelineType, config);
+
+  const jobsWithSortedProps = Object.fromEntries(
+    Object.entries(jobs).map(([jobName, job]) => [
+      jobName,
+      sortGitLabJobDefProps(job),
+    ]),
+  );
+
+  return {
+    image,
+    stages,
+    variables,
+    workflow,
+    ...pipelineRest,
+    // jobs need to be spread into main YAML, because GitLab pipeline YAML has no jobs key - jobs are top level with their key as their name
+    ...jobsWithSortedProps,
+  };
+}
+
 export async function generatePipelineFiles<T extends PipelineType>(
   config: Config,
   pipelineType: T,
   mode: PipelineMode<T>,
 ) {
   if (mode === "childpipeline") {
-    const trigger = getPipelineTriggerForGitlabChildPipeline();
-
-    const { jobs, image, stages, variables, workflow, ...pipelineRest } =
-      await createChildPipeline(pipelineType, trigger, config);
-    const jobsWithSortedProps = Object.fromEntries(
-      Object.entries(jobs).map(([jobName, job]) => [
-        jobName,
-        sortGitLabJobDefProps(job),
-      ]),
+    const pipelineContent = await generateChildPipelineContent(
+      config,
+      pipelineType,
     );
-    await writeYamlfile(`__pipeline.yml`, {
-      image,
-      stages,
-      variables,
-      workflow,
-      ...pipelineRest,
-      // jobs need to be spread into main YAML, because GitLab pipeline YAML has no jobs key - jobs are top level with their key as their name
-      ...jobsWithSortedProps,
-    });
-  } else {
-    const { jobs, image, stages, variables, workflow, ...pipelineRest } =
-      await createMainPipeline(pipelineType, config);
-    const jobsWithSortedProps = Object.fromEntries(
-      Object.entries(jobs).map(([jobName, job]) => [
-        jobName,
-        sortGitLabJobDefProps(job),
-      ]),
-    );
-    await writeYamlfile(`.gitlab-ci.yml`, {
-      image,
-      stages,
-      variables,
-      workflow,
-      ...pipelineRest,
-      // jobs need to be spread into main YAML, because GitLab pipeline YAML has no jobs key - jobs are top level with their key as their name
-      ...jobsWithSortedProps,
-    });
+    return writeYamlfile(`__pipeline.yml`, pipelineContent);
   }
+  const pipelineContent = await generateLocalPipelineContent(
+    config,
+    pipelineType,
+  );
+  return writeYamlfile(`.gitlab-ci.yml`, pipelineContent);
 }
