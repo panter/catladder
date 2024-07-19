53d0f1f62ae211423e686d47c580d3119368b4d5
Marco Wettstein <maw@panter.ch>
2024-07-19 12:01:22 +0200

    chore: fix flaky test

--- pipeline/examples/rails-k8s-with-worker-dockerfile.test.ts
+++ pipeline/examples/rails-k8s-with-worker-dockerfile.test.ts
@@ -1,9 +1,26 @@
-import { rmSync, writeFileSync } from "fs";
+import { mkdirSync, rmSync, writeFileSync } from "fs";
 import { createYamlLocalPipeline } from "./__utils__/helpers";
 import config from "./rails-k8s-with-worker";
+import { merge } from "lodash";
+import { join } from "path";
 
 it("matches snapshot with a Dockerfile", async () => {
-  writeFileSync("Dockerfile", "");
-  expect(await createYamlLocalPipeline(config)).toMatchSnapshot();
-  rmSync("Dockerfile");
+  const appDir = ".temp-with-dockerfile";
+  mkdirSync(appDir);
+  writeFileSync(join(appDir, "Dockerfile"), "");
+  expect(
+    await createYamlLocalPipeline(
+      merge(config, {
+        components: {
+          app: {
+            dir: appDir,
+          },
+        },
+      }),
+    ),
+  ).toMatchSnapshot();
+
+  rmSync(appDir, {
+    recursive: true,
+  });
 });
